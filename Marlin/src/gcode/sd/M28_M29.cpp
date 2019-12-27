18ed9550c1b75bab16cfd9354d7e4f5a9638693a
Thiago Borges <thborges@gmail.com>
2019-12-27 15:18:05 -0300

    A lightweight binary compressed protocol for sending SD files through SERIAL port.
    Send M28 !filename to enable the protocol and use a client to upload the file.
    Compression uses the already present heatshrink library.

--- Marlin/src/gcode/sd/M28_M29.cpp
+++ Marlin/src/gcode/sd/M28_M29.cpp
@@ -21,15 +21,136 @@
  */
 
 #include "../../inc/MarlinConfig.h"
+#include "../../module/temperature.h"
 
 #if ENABLED(SDSUPPORT)
 
 #include "../gcode.h"
 #include "../../sd/cardreader.h"
 
+#define BCT_COMPRESSION
+
+#if ENABLED(BCT_COMPRESSION)
+  #include "../../libs/heatshrink/heatshrink_decoder.h"
+#endif
+
 #if NUM_SERIAL > 1
   #include "../queue.h"
 #endif
+
+#define BCT_WAIT_BYTE_TIMEOUT 5000
+#define BCT_MAX_RAW_SIZE 512
+#define BCT_EOT 4
+#define BCT_ACK 6
+#define BCT_NCK 21
+#define BCT_CAN 24
+
+inline uint16_t serial_read_bytes(uint8_t *buff, uint16_t count, uint8_t *chksum) {
+
+  (*chksum) = 0;
+  for(uint16_t i = 0; i < count; i++) {
+    int c;
+    millis_t start = millis();
+    while ((c = MYSERIAL0.read()) < 0) {// wait for char
+      thermalManager.manage_heater();
+      if ((millis() - start) > BCT_WAIT_BYTE_TIMEOUT) {
+        // timeout, finalize transmission
+        SERIAL_CHAR(BCT_EOT);
+        SERIAL_ECHOLN("Timeout waiting for binary data.");
+        return i;
+      }
+    }
+    buff[i] = c;
+    (*chksum) ^= c;
+  }
+  return count;
+}
+
+int16_t poll_output(heatshrink_decoder *hsd, uint8_t *output_buffer, size_t *polled) {
+  HSD_poll_res pres;
+  do {
+    size_t consumed;
+    pres = heatshrink_decoder_poll(hsd, &output_buffer[*polled], 
+      BCT_MAX_RAW_SIZE - *polled, &consumed);
+    *polled += consumed;
+    if (*polled == BCT_MAX_RAW_SIZE) {
+      int16_t ret = card.write(output_buffer, BCT_MAX_RAW_SIZE);
+      if (ret < 0)
+        return ret;
+      *polled = 0;
+    }
+  } while (pres == HSDR_POLL_MORE);
+  return 0;
+}
+
+inline void receive_raw_file() {
+
+  struct [[gnu::packed]] block_header {
+    union {
+      struct {
+        uint16_t size;
+        uint8_t last_block;
+        uint8_t chksum;
+      };
+      char csize[2];
+    };
+  } bh;
+  uint8_t input_buffer[BCT_MAX_RAW_SIZE];
+
+  // decoder setup
+  heatshrink_decoder hsd;
+  uint8_t output_buffer[BCT_MAX_RAW_SIZE];
+  heatshrink_decoder_reset(&hsd);
+
+  size_t polled = 0;
+  do {
+    uint8_t chksum;
+    uint16_t bread = serial_read_bytes((uint8_t*)&bh, sizeof(bh), &chksum);
+    if (bread != sizeof(bh))
+      break;
+
+    bread = serial_read_bytes(input_buffer, bh.size, &chksum);
+    if (bread != bh.size)
+      break;
+    
+    if (chksum != bh.chksum) { // checksum differ, request again
+      SERIAL_CHAR(BCT_NCK);
+      continue;
+    }
+
+    // decompress bytes received
+    size_t sunk = 0, consumed;
+    while (sunk < bread) {
+      heatshrink_decoder_sink(&hsd, &input_buffer[sunk], BCT_MAX_RAW_SIZE - sunk, &consumed);
+      sunk += consumed;
+      if (poll_output(&hsd, output_buffer, &polled) < 0) {
+        SERIAL_CHAR(BCT_CAN);
+        return;
+      }
+    }
+    // block processed successfully
+    SERIAL_CHAR(BCT_ACK);
+
+    if (bh.last_block) {
+      HSD_finish_res fres = heatshrink_decoder_finish(&hsd);
+      if (fres == HSDR_FINISH_MORE) {
+        if (poll_output(&hsd, output_buffer, &polled) < 0)
+          break;
+      }
+      if (polled > 0) { // write remaining bytes in output_buffer, if any
+        if (card.write(output_buffer, polled) < 0)
+          break;
+      }
+
+      // signal end of transmission
+      SERIAL_CHAR(BCT_EOT);
+      return;
+    }
+  } while (1); // receiving file content
+
+  // on error, report cancel transmission
+  SERIAL_CHAR(BCT_CAN);
+}
 
 /**
  * M28: Start SD Write
@@ -58,8 +179,20 @@ void GcodeSuite::M28() {
 
   #else
 
-    card.openFileWrite(parser.string_arg);
+    // raw data protocol, enabled by M28 !file_name
+    bool saving_raw = false;
+    char *file = parser.string_arg;
+    if (file[0] == '!') {
+      saving_raw = true;
+      file++;
+    }
 
+    card.openFileWrite(file);
+
+    if (card.flag.saving && saving_raw) {
+      receive_raw_file();
+      card.closefile();
+    }
   #endif
 }
 
